741a7e206a8cce5dac85f419c05ebd4d3edb783b
mac229 <mac229@interia.eu>
2017-01-02 00:18:39 +0100

    Payment view

---
+++ src/bsr/client/ui/PaymentController.java
@@ -0,0 +1,48 @@
+package bsr.client.ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Created by Maciej on 2017-01-02.
+ */
+public class PaymentController {
+
+    @FXML
+    private TextField amountText;
+
+    @FXML
+    private Text resultText;
+
+    @FXML
+    protected void onPaymentOutClick(ActionEvent event) throws IOException {
+        System.out.println(amountText.getText());
+    }
+
+    @FXML
+    protected void onPaymentInClick(ActionEvent event) throws IOException {
+        System.out.println(amountText.getText());
+    }
+
+    @FXML
+    protected void onBackClick(ActionEvent event) throws IOException {
+        showHome(event);
+    }
+
+    private void showHome(ActionEvent event) throws IOException {
+        Parent view = FXMLLoader.load(getClass().getResource("../fxml/home.fxml"));
+        Scene scene = new Scene(view);
+        Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        appStage.setScene(scene);
+        appStage.show();
+    }
+}
