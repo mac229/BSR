067165e90de77d28ee2195fb182cbf64a15b6ebb
mac229 <mac229@interia.eu>
2017-01-02 00:18:50 +0100

    Transfer view

---
+++ src/bsr/client/ui/TransferController.java
@@ -0,0 +1,57 @@
+package bsr.client.ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Maciej on 2017-01-02.
+ */
+public class TransferController {
+
+    @FXML
+    private TextField senderText;
+
+    @FXML
+    private TextField receiverText;
+
+    @FXML
+    private TextField amountText;
+
+    @FXML
+    private TextField titleText;
+
+    @FXML
+    private Text resultText;
+
+    @FXML
+    public void onTransferClick(ActionEvent event) {
+        System.out.println(senderText.getText());
+        System.out.println(receiverText.getText());
+        System.out.println(amountText.getText());
+        System.out.println(titleText.getText());
+    }
+
+    @FXML
+    protected void onBackClick(ActionEvent event) throws IOException {
+        showHome(event);
+    }
+
+    private void showHome(ActionEvent event) throws IOException {
+        Parent view = FXMLLoader.load(getClass().getResource("../fxml/home.fxml"));
+        Scene scene = new Scene(view);
+        Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        appStage.setScene(scene);
+        appStage.show();
+    }
+}
