69d25091dbf4cceea53ae32bebb2fbaf3c8b8061
mac229 <mac229@interia.eu>
2017-01-06 21:38:58 +0100

    Account

---
+++ src/bsr/server/model/Account.java
@@ -0,0 +1,13 @@
+package bsr.server.model;
+
+import java.util.List;
+
+/**
+ * Created by Maciej on 2017-01-06.
+ */
+public class Account {
+
+    private String login;
+    private String password;
+    private List<Bill> bills;
+}
